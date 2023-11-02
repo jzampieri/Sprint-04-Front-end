ca178181ca24552f756f4bb4772137581972f533
Julio <juliocezampieri@gmail.com>
2023-11-02 19:15:10 -0300

    Header page

---
+++ index.js
@@ -0,0 +1,13 @@
+var lastScroll = 0;
+
+$(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+
+    if (scroll > lastScroll) {
+        $('.header-page').addClass('menu-animation');
+    } if (scroll < 1) {
+        $('.header-page').removeClass('menu-animation');
+    }
+
+    lastScroll = scroll;
+});
