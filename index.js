89a838deadb4a49fe39d6cf265c81ebedb164578
Julio <juliocezampieri@gmail.com>
2023-11-04 22:55:27 -0300

    Page Serviços DONE

---
+++ index.js
@@ -0,0 +1,14 @@
+var lastScroll = 0;
+
+$(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+
+    if (scroll > lastScroll) {
+        $('.header-page').addClass('menu-animation');
+    } else if (scroll < 1) {
+        $('.header-page').removeClass('menu-animation');
+    }
+
+    lastScroll = scroll;
+});
+
